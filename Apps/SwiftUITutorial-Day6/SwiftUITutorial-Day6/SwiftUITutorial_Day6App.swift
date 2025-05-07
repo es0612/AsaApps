9953d3487e8e9d9f65fb8df9e2111694ae7cab0e
Shinya <emashinya0612@gmail.com>
2025-05-07 10:05:56 +0900

    day6 setup

---
+++ Apps/SwiftUITutorial-Day6/SwiftUITutorial-Day6/SwiftUITutorial_Day6App.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUITutorial_Day6App.swift
+//  SwiftUITutorial-Day6
+//  
+//  Created on 2025/05/07
+//
+
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial_Day6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
