cfbf3ca3eaa02cd86f2321236b5a12bb53858985
Shinya <emashinya0612@gmail.com>
2025-08-24 15:34:34 +0900

    bug:color settings

--- Shared/AsaCard.swift
+++ Shared/AsaCard.swift
@@ -21,9 +21,9 @@ struct AsaCard_Previews: PreviewProvider {
         AsaCard {
             Text("サンプルテキスト")
                 .font(.body.weight(.medium))
-                .foregroundColor(.asaCoffeeBrown)
+                .foregroundColor(Color("asaCoffeeBrown"))
         }
         .padding()
-        .background(.asaSoftCream)
+        .background(Color("asaSoftCream"))
     }
 }
